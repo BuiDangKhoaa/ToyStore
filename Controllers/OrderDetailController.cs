6005dc1c3a14fa801549af09fa58dd56bd3096a4
Bùi Đăng Khoa <21dh113768@st.huflit.edu.vn>
2022-11-20 00:01:05 +0700

    changed

--- Controllers/OrderDetailController.cs
+++ Controllers/OrderDetailController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace ToyStore.Controllers
     {
         DBSportStoreEntities db = new DBSportStoreEntities();
         // GET: OrderDetail
+
         public ActionResult GroupByTop5()
         {
             List<OrderDetail> orderD = db.OrderDetails.ToList();
@@ -34,6 +36,33 @@ namespace ToyStore.Controllers
                             Sum_Quantity = gr.Sum(s => s.Quantity)
                         };
             return View(query.Take(5).ToList());
+        }
+        public ActionResult Test(string category, int? page)
+        {
+            List<OrderDetail> orderD = db.OrderDetails.ToList();
+            List<Product> proList = db.Products.ToList();
+            var query = from od in orderD
+                        join p in proList on od.IDProduct equals p.ProductID into tbl
+                        group od by new
+                        {
+                            idPro = od.IDProduct,
+                            namePro = od.Product.NamePro,
+                            imagePro = od.Product.ImagePro,
+                            price = od.Product.Price
+                        } into gr
+                        orderby gr.Sum(s => s.Quantity) descending
+                        select new ViewModel
+                        {
+                            IDPro = gr.Key.idPro,
+                            NamePro = gr.Key.namePro,
+                            ImgPro = gr.Key.imagePro,
+                            pricePro = (decimal)gr.Key.price,
+                            Sum_Quantity = gr.Sum(s => s.Quantity)
+                        };
+            return PartialView(query.Take(5).ToList());
+
+
+
         }
     }
 }
